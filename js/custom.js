0736829984a8e08424c4c6fa949d838e80e86472
atelier601 <franck-artaud@atelier601.com>
2018-11-16 16:34:13 +0100

    decouverte du JS

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+/* Show and hide menu */
+
+$(document).ready(function(){
+    
+    'use strict';
+    $(window).scroll(function(){
+    
+        'use strict';
+        if($(window)).scrollTop() < 80 ){
+            
+            $('.navbar').css ({
+                'margin-top':'-100px',
+                'opacity':'0'
+            });
+            
+            $('.navbar-default').css({
+                'background-color': 'rgba(59, 59, 59, 0)'
+            });
+            
+        } else {
+            $('.navbar').css ({
+                'margin-top':'0px',
+                'opacity':'1'
+            });
+            
+         $('.navbar-default').css({
+                'background-color': 'rgba(59, 59, 59, 0.7)',
+                'border-color':'#444'
+        });
+    }
+    
+});
